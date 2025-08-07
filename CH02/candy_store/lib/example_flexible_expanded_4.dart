f74001f6d25265ae96d52ec3005c56632484cd5f
ZARRAR-1 <mack4009@gmail.com>
2025-08-07 12:31:30 +0500

    Added Comment to Add SingleChildScrollView and set the Scroll Direction to Horizontal

--- CH02/candy_store/lib/example_flexible_expanded_4.dart
+++ CH02/candy_store/lib/example_flexible_expanded_4.dart
@@ -4,6 +4,7 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        //So we can wrap the Row with SingleChildScroll view and set the Scroll Direction to Horizontal
         body: Row(
           children: [
             const Text('First text'),
